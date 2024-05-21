f10b8b29ae1e1dc9d35b755b4fc8d699f21e5be4
BlazEffect <kamalinigor@gmail.com>
2024-05-21 20:38:32 +0300

    Added endpoint for adding a review

--- app/Http/Controllers/Api/v1/ReviewController.php
+++ app/Http/Controllers/Api/v1/ReviewController.php
@@ -38,6 +38,61 @@ class ReviewController extends BaseApiController
     }
 
     /**
+     * @OA\Post (
+     *     path="/review/create",
+     *     tags={"Reviews"},
+     *     summary="Добавление отзыва",
+     *     description="Добавление отзыва",
+     *     security={
+     *         { "Bearer":{} }
+     *     },
+     *     @OA\RequestBody(
+     *         @OA\JsonContent(
+     *             allOf={
+     *                 @OA\Schema(
+     *                     @OA\Property(property="name", type="string", example="Название отзыва"),
+     *                     @OA\Property(property="text", type="string", example="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi laborum magnam maxime quia ut? Aperiam autem"),
+     *                     @OA\Property(property="rating_value", type="int", example=1),
+     *                 )
+     *             }
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Успешно",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="success", type="boolean", example="true"),
+     *             @OA\Property(property="data", type="object", ref="#/components/schemas/Review"),
+     *             @OA\Property(property="message", type="string", example="Заказ успешно создан.")
+     *         )
+     *     )
+     * )
+     *
+     * @param Request $request
+     * @return ApiErrorResponse|ApiSuccessResponse
+     */
+    public function createReview(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'string|required',
+            'text' => 'string|required',
+            'rating_value' => 'int|required',
+        ]);
+
+        if ($validator->fails()) {
+            return new ApiErrorResponse('Ошибки валидации.', $validator->errors()->toArray());
+        }
+
+        $data = $request->all();
+        $data['user_id'] = auth()->user()->id;
+        $data['status'] = 0;
+
+        $review = Review::create($data);
+
+        return new ApiSuccessResponse($review, 'Отзыв был создан.');
+    }
+
+    /**
      * @OA\Patch(
      *     path="/review/{reviewId}",
      *     tags={"Reviews"},
