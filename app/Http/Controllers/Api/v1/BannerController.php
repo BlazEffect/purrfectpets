7ff1a1dafe6f1bade409b0e709ffd2e73426ac9a
BlazEffect <kamalinigor@gmail.com>
2024-05-21 18:34:54 +0300

    Put the banner scheme into a separate file

--- app/Http/Controllers/Api/v1/BannerController.php
+++ app/Http/Controllers/Api/v1/BannerController.php
@@ -6,22 +6,6 @@ use App\Http\Responses\ApiSuccessResponse;
 use App\Models\Banner;
 use OpenApi\Annotations as OA;
 
-/**
- * @OA\Schema(
- *     schema="Banner",
- *     required={"id", "name"},
- *     @OA\Property(property="id", type="integer", example=1),
- *     @OA\Property(property="name", type="string", example="Название для администратора"),
- *     @OA\Property(property="title", type="string", example="Отображаемое название"),
- *     @OA\Property(property="image", type="string", example="Картинка "),
- *     @OA\Property(property="link", type="string", example="Ссылка баннера"),
- *     @OA\Property(property="text", type="text", example="Текст баннера"),
- *     @OA\Property(property="active", type="boolean", example=true),
- *     @OA\Property(property="order", type="integer", example=1),
- *     @OA\Property(property="created_at", type="string", format="date-time", example="2024-04-08 00:00:00"),
- *     @OA\Property(property="updated_at", type="string", format="date-time", example="2024-04-08 00:00:00")
- * )
- */
 class BannerController extends BaseApiController
 {
     /**
---
+++ app/Virtual/Models/Banner.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Virtual\Models;
+
+use DateTime;
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Schema(
+ *     title="Banner",
+ *     description="Banner model",
+ *     @OA\Xml(
+ *         name="Banner"
+ *     )
+ * )
+*/
+class Banner
+{
+    /**
+     * @OA\Property(
+     *     title="ID",
+     *     description="ID",
+     *     format="int128",
+     *     example=1
+     * )
+     *
+     * @var int $id
+     */
+    private int $id;
+
+    /**
+     * @OA\Property(
+     *     title="Name",
+     *     description="Name of banner",
+     *     example="Название для администратора"
+     * )
+     *
+     * @var string $name
+     */
+    public string $name;
+
+    /**
+     * @OA\Property(
+     *     title="Title",
+     *     description="Title of banner",
+     *     example="Отображаемое название"
+     * )
+     *
+     * @var string|null $title
+     */
+    public ?string $title;
+
+    /**
+     * @OA\Property(
+     *     title="Image",
+     *     description="Image",
+     *     example="gnuithiugh598.png"
+     * )
+     *
+     * @var string $image
+     */
+    public string $image;
+
+    /**
+     * @OA\Property(
+     *     title="Url",
+     *     description="Link where the banner will lead",
+     *     example="/catalog"
+     * )
+     *
+     * @var string|null $url
+     */
+    public ?string $link;
+
+    /**
+     * @OA\Property(
+     *     title="Text",
+     *     description="Text of the new banner",
+     *     example="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque aut consequuntur delectus"
+     * )
+     *
+     * @var string|null $text
+     */
+    public ?string $text;
+
+    /**
+     * @OA\Property(
+     *     title="Active",
+     *     example=true
+     * )
+     *
+     * @var bool $active
+     */
+    public bool $active;
+
+    /**
+     * @OA\Property(
+     *     title="Order",
+     *     description="Element display order",
+     *     example=0
+     * )
+     *
+     * @var int $order
+     */
+    public int $order;
+
+    /**
+     * @OA\Property(
+     *     title="Created at",
+     *     description="Created at",
+     *     example="2024-04-08 00:00:00",
+     *     format="datetime",
+     *     type="string"
+     * )
+     *
+     * @var DateTime|null $created_at
+     */
+    private ?DateTime $created_at;
+
+    /**
+     * @OA\Property(
+     *     title="Updated at",
+     *     description="Updated at",
+     *     example="2024-04-08 00:00:00",
+     *     format="datetime",
+     *     type="string"
+     * )
+     *
+     * @var DateTime|null $updated_at
+     */
+    private ?DateTime $updated_at;
+}
