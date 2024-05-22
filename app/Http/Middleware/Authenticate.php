cc5a86fe7150105abca1ad6e4da5054e66128841
BlazEffect <kamalinigor@gmail.com>
2024-05-22 17:29:49 +0300

    Changed the response for unauthorized users

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Middleware;
 
+use App\Http\Responses\ApiErrorResponse;
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class Authenticate extends Middleware
 {
@@ -13,5 +17,18 @@ class Authenticate extends Middleware
     protected function redirectTo(Request $request): ?string
     {
         return $request->expectsJson() ? null : route('login');
+    }
+
+    public function handle($request, Closure $next, ...$guards): ApiErrorResponse|JsonResponse
+    {
+        foreach ($guards as $guard) {
+            if ($this->auth->guard($guard)->check()) {
+                $this->auth->shouldUse($guard);
+
+                return $next($request);
+            }
+        }
+
+        return new ApiErrorResponse('Вы не авторизованы.', [], Response::HTTP_UNAUTHORIZED);
     }
 }
