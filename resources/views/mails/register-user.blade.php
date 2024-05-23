f592a4b480f5515e4bee5843b9c55f18e159f2c2
BlazEffect <kamalinigor@gmail.com>
2024-05-23 13:58:05 +0300

    Created a class for messages when a user registers

---
+++ resources/views/mails/register-user.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    </head>
+
+    <body>
+        <h3>Вы зарегестироваровались на сайте PurrfectPets</h3>
+        <br>
+        <p>Ваш логин: <b>{{ $user->email }}</b></p>
+        <p>Ваш пароль: <b>{{ $password }}</b></p>
+        <br>
+        <p>Ваши данные профиля:</p>
+        <p>ФИО: {{ $userProfile['first_name'] }} {{ $userProfile['surname'] }} {{ $userProfile['last_name'] }}</p>
+        <p>Почта: {{ $user->email }}</p>
+        <p>Номер телефона: {{ $userProfile['phone'] }}</p>
+    </body>
+</html>
