304691419baf7f399a829b448cd7328782d15ba4
Annatk057 <annatk990@gmail.com>
2024-06-14 16:43:34 +0300

    add files

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import 'dotenv/config'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  server: {
+    host: process.env.SERVER_IP,
+    port: process.env.SERVER_PORT,
+    proxy: {
+      '/api': {
+        target: process.env.API_SERVER_URL,
+        changeOrigin: true,
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
