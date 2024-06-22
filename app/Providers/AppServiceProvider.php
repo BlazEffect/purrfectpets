6069f058fd34b5864b31a846c11b28cc32f3ed99
BlazEffect <kamalinigor@gmail.com>
2024-06-22 20:04:14 +0300

    Added service registration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use App\Services\AuthService;
 use App\Services\BrandService;
 use App\Services\EmailService;
 use App\Services\MenuService;
+use App\Services\OrderService;
+use App\Services\ProductService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,13 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(EmailService::class, fn() => new EmailService());
         $this->app->singleton(BrandService::class, fn() => new BrandService());
         $this->app->singleton(MenuService::class, fn() => new MenuService());
+        $this->app->singleton(OrderService::class, function ($app) {
+            return new OrderService(
+                $app->make(ProductService::class),
+                $app->make(EmailService::class)
+            );
+        });
+        $this->app->singleton(ProductService::class, fn() => new ProductService());
     }
 
     /**
